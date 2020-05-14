3804787f544c34bf18b1491bd66ae266a1f62986
kaikina <girou.tom@gmail.com>
2020-05-14 20:34:25 +0200

    [3] Create pokemon details page

--- pages/pokemons/_id.vue
+++ pages/pokemons/_id.vue
@@ -1,11 +1,71 @@
 <template>
-
+  <section>
+    <div class="columns is-centered">
+      <div class="column is-half">
+        <h1 class="title">{{ this.details.name.charAt(0).toUpperCase() + this.details.name.slice(1) }}</h1>
+      </div>
+    </div>
+    <div class="columns is-centered">
+      <div class="column">
+        <figure class="image is-96x96 is-pulled-right">
+          <img :src="this.details.sprites.front_default" alt="pokemon image">
+        </figure>
+      </div>
+      <div class="column">
+        <p>{{ this.description }}</p>
+        <div class="columns">
+          <div class="column">
+            <p v-if="!loading"><strong>Taille</strong> <br>
+              {{ this.details.height / 10 }} m</p>
+          </div>
+          <div class="column">
+            <p><strong>Catégorie</strong><br>
+              {{ this.genus }}</p>
+          </div>
+        </div>
+        <div class="columns">
+          <div class="column">
+            <p><strong>Poids</strong> <br>
+              {{ this.details.weight / 10 }} kg</p>
+          </div>
+          <div class="column">
+            <p><strong>Talent</strong><br>
+              {{ this.ability.charAt(0).toUpperCase() + this.ability.slice(1) }}</p>
+          </div>
+        </div>
+      </div>
+      <div class="column"></div>
+    </div>
+  </section>
 </template>
 
 <script>
-    export default {
-        name: "_id"
+
+  export default {
+    data () {
+      return {
+        id: this.$route.params.id,
+        details: {},
+        description: null,
+        genus: null,
+        ability: null
+      }
+    },
+    head () {
+      return {
+        title: this.details.name.charAt(0).toUpperCase() + this.details.name.slice(1)
+      }
+    },
+    async asyncData ({params, $axios}) {
+      let details = await $axios.$get(`https://pokeapi.co/api/v2/pokemon/${params.id}`);
+      const ability = details.abilities.find(ability => ability.slot === 1);
+      let specie = await $axios.$get(details.species.url);
+      const flavor = specie.flavor_text_entries.find(entry => entry.language.name === 'en');
+      const genera = specie.genera.find(genus => genus.language.name === "en");
+      return {details : details, ability: ability.ability.name, description: flavor.flavor_text,
+        genus: genera.genus, loading: false}
     }
+  }
 </script>
 
 <style scoped>
